d5df02c9e90129ccc2179f835cfd1272c74fb6a5
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-12 17:04:28 +0000

    暗号化修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -10,19 +10,24 @@ class TopController < ApplicationController
     
     def login
       user = User.find_by(uid: params[:uid])
+      
       if user && BCrypt::Password.new(user.pass) == params[:pass]
+        # 認証成功
         session[:login_uid] = user.uid
-        redirect_to top_main_path
+        redirect_to action: :main
       else
+        # 認証失敗
         render "error", status: 422
       end
     end
 
 
 
+
     
     def logout
-        session.delete(:login_uid)
-        redirect_to action: "main"
+      session.delete(:login_uid)
+      redirect_to action: :main
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include BCrypt
+
+  # パスワード設定時に自動でハッシュ化
+  def password=(new_pass)
+    self.pass = Password.create(new_pass)
+  end
+
+  # ハッシュ化されたパスワードを比較用に取得
+  def password
+    Password.new(self.pass)
+  end
 end
+
