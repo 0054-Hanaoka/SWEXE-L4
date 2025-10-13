efa11ab779e7531928c94652cd053205ca5495d2
Hiroki Hanaoka <2311140054k@ed.fuk.kindai.ac.jp>
2025-10-13 09:46:21 +0000

    暗号化の完成

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,24 +1,27 @@
 class TopController < ApplicationController
-    def main
-        if session[:login_uid] != nil
-            render "main"
-            
-        else
-            render "login"
-        end
+  def main
+    if session[:login_uid]
+      render "main"
+    else
+      render "login"
     end
-    
-    def login
-        if params[:uid] == "kindai" and params[:pass] == "sanriko"
-            session[:login_uid] = params[:uid]
-            redirect_to top_main_path
-        else
-            render "error",status: 422
-        end
+  end
+
+  def login
+    # DBから該当ユーザーを検索
+    user = User.find_by(uid: params[:uid])
+
+    if user != nil and BCrypt::Password.new(user.pass) == params[:pass]
+      session[:login_uid] = params[:pass]
+      redirect_to top_main_path
+    else
+      render "error", status: 422
     end
-    
-    def logout
-        session.delete(:login_uid)
-        redirect_to action: "main"
-    end
+  end
+
+  def logout
+    session.delete(:login_uid)
+    redirect_to top_main_path
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,6 +17,10 @@ Rails.application.routes.draw do
   post 'top/login'
   get 'top/logout'
   root 'top#main'
+  get 'users/index'
+  get 'users/new'
+  
+  resources :users
   
   
 end
